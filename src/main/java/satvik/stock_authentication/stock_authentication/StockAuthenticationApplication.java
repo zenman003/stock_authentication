752cadbce0cbb3ad90470e886938271762d56486
satviknasa <satviknasa8@gmail.com>
2024-09-10 00:53:38 +0530

    Initial commit

---
+++ src/main/java/satvik/stock_authentication/stock_authentication/StockAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package satvik.stock_authentication.stock_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/satvik/stock_authentication/stock_authentication/StockAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package satvik.stock_authentication.stock_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
